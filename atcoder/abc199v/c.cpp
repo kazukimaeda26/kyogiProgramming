d0f1f58fed62ec719b8ff8f1a5c375c92bf72831
kazukimaeda26 <kazukimaeda26@gmail.com>
2021-05-19 21:54:52 +0900

    実際にflipさせない.flipする回数を数えておくのがpoint!

--- atcoder/abc199v/c.cpp
+++ atcoder/abc199v/c.cpp
@@ -2,29 +2,35 @@
 using namespace std;
 
 int main(){
-  int n, q;
+  int n;
   string s;
-  cin >> n >> s >> q;
-  vector<vector<int>> vec(q, vector<int>(3));
+  cin >> n >> s;
+  int q;
+  cin >> q;
+  int fl = 0;
+  int n2 = n*2;
   for(int i=0; i<q; i++){
-    for(int j=0; j<3; j++){
-      cin >> vec[i][j];
+    int t, a, b;
+    cin >> t >> a >> b;
+    if(t==1){
+      // 0からのスタートに揃える
+      --a; --b;
+      //flipしている状態なら,入れ替える位置をうまいこと調整する。
+      // (ex)n=4, a=5, b=7だとるすると, 1番目と3番目をswapしたい。n足して2nで割ったときのあまりとするとよい。
+      if(fl) a=(a+n)%n2;
+      if(fl) b=(b+n)%n2;
+      swap(s[a],s[b]);
+    } else {
+      //0と1の反転
+      fl ^= 1;
     }
   }
-  for(int i=0; i<q; i++){
-    if(vec[i][0]==1){
-      for(int j=0; j<s.length(); j++){
-        if(j == vec[i][1]){
-          char tmp = s[j];
-          s[j] = s[vec[i][2]];
-          s[vec[i][2]] = tmp;
-        }
-        else s[j] = s[j];
-      }
-    }
-    else{
-
-    }
+  if (fl) {
+    // 0番目からn個取り出す
+    string s1 = s.substr(0,n);
+    // n番目以降全て取り出す
+    string s2 = s.substr(n);
+    s = s2+s1;
   }
   cout << s << endl;
 }
